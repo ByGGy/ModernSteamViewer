76be50896121bde590b32e564c1e22442ac7b122
BM <benjamin.manderscheid@analogway.com>
2017-10-06 17:37:09 +0200

    Added a button to delete a bookmark from the bookmark View

--- src/containers/bookmarkView.jsx
+++ src/containers/bookmarkView.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import styled from 'styled-components'
+
+import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 
+import DeleteIcon from '../icons/deleteIcon'
+
+import { RemoveBookmark } from '../reducers/bookmarkReducer'
 import { FetchNewsRequest } from '../sagas/steamSaga'
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  justify-items: stretch;
+`
+
+const RemoveArea = styled.div`
+  grid-row: 1;
+  grid-column: 1;
+  overflow: hidden;
+`
+
+const SelectArea = styled.div`
+  grid-row: 1;
+  grid-column: 2;
+  overflow: hidden;
+`
 
 class BookmarkView extends Component {
   constructor(props) {
     super(props)
 
+    this.handleRemove = this.handleRemove.bind(this)
     this.handleSelection = this.handleSelection.bind(this)
+  }
+
+  handleRemove(event) {
+    const { app } = this.props
+    this.props.dispatch(RemoveBookmark(app))
   }
 
   handleSelection(event) {
@@ -21,13 +51,24 @@ class BookmarkView extends Component {
     const { app } = this.props
 
     return (
-      <RaisedButton
-        style={{margin: '4px'}}
-        buttonStyle={{textAlign: 'left', overflow: 'hidden'}}
-        fullWidth
-        label={app.name}
-        onClick={this.handleSelection}
-      />
+      <Layout>
+        <RemoveArea>
+          <FlatButton
+            style={{minWidth: '0px', height:'100%'}}
+            icon={<DeleteIcon />}
+            onClick={this.handleRemove}
+          />
+        </RemoveArea>
+        <SelectArea>
+          <RaisedButton
+            style={{margin: '4px'}}
+            buttonStyle={{textAlign: 'left', overflow: 'hidden'}}
+            fullWidth
+            label={app.name}
+            onClick={this.handleSelection}
+          />
+        </SelectArea>
+      </Layout>
     )
   }
 }
---
+++ src/icons/deleteIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SvgIcon from 'material-ui/SvgIcon'
+
+const DeleteIcon = (props) => (
+  <SvgIcon {...props} viewBox="0 0 32 32" color="#439EBF">
+    <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
+  </SvgIcon>
+)
+
+export default DeleteIcon
